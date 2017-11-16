c331c7b3a762e9e65255e67058cb2e159af33bc6
Andrew Mangeni <alm2255@columbia.edu>
2017-11-16 19:58:09 +0000

    adding music groups dictionary

---
+++ make_groups.py
@@ -0,0 +1,149 @@
+groups = {}
+
+#Group 0
+groups['duet'] = 000
+
+#Group 1
+groups['classical'] = 001
+groups['classic'] = 001
+groups['classical'] = 
+groups['harpsichord'] = 
+groups['harpsicord'] = 
+groups['hard'] = 
+groups['flute'] = 
+groups['violin'] = 
+groups['violins'] = 
+groups['piano solo'] = 
+groups['keyboard'] = 
+groups['flutes'] = 
+groups['cello'] = 
+groups['clarinet'] = 
+groups['orchestra'] = 
+groups['orchestral'] = 
+groups['viola'] = 
+groups['harp'] = 
+groups['strings'] = 
+groups['string'] = 
+groups['organ'] = 
+groups['woodwind'] = 
+
+
+1.  duet
+
+ 
+
+2.  Classical, classic, classical, harpsichord, harpsicord, hard, flute, violin, violins, piano solo, keyboard, flutes, cello, clarinet, orchestra, orchestral, viola, harp, strings, string, organ, woodwind
+
+ 
+
+3.  Techno, electronic, electric, electro, electronica, house
+
+ 
+
+4.  Ambient, echo, eerie, drone, calm, water, trance, birds, spacey, space
+
+ 
+
+5.  Opera, female opera, male opera, operatic, soprano, medieval, baroque, lute
+
+ 
+
+6.  Funky, funk, disco, dance, synth, synthesizer
+
+ 
+
+7.  pop, dance
+
+ 
+
+8.  Monks, chant, chanting,
+
+ 
+
+9.  Jazz, jazzy, sax, blues
+
+ 
+
+ 
+
+10.Oriental, indian, india, eastern, sitar, middle eastern, arabic
+
+ 
+
+11.Rap, hip hop
+
+ 
+
+12.Punk, rock, hard rock, soft rock, electric guitar
+
+ 
+
+13.Acoustic, acoustic guitar, classical guitar
+
+ 
+
+14.Celtic, irish
+
+ 
+
+15.Choir, chorus, choral,
+
+ 
+
+16. jungle, bongos, reggae, tribal
+
+ 
+
+17.beats, percussion, beat
+
+ 
+
+18.fiddle, banjo, country
+
+ 
+
+19.modern, new age
+
+ 
+
+20.metal, heavy, heavy metal
+
+ 
+
+21.folk, lute
+
+ 
+
+22.trumpet, horns, horn
+
+ 
+
+ 
+
+ 
+
+{“funk”:5:25, 2:6,
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+, , , , , drums, , , fast, piano, , , vocal, , female, , , male, , , , , , , , , , , , , , , , , , , female voice, ,
+
+, drum, , , , , , instrumental, bass, , no piano, , , , , , , , , , , , , , , spanish, , , no guitar, , male singer, , , , , , , dark, , , , , , , bells, , , , singer, , , , , , , wind, , , , , oboe, , , , , , , hard, mellow, , fast beat, , , , no violin, , , , , , , chimes, , , , , , industrial, , plucking, , , , , , repetitive, airy, world, , deep, , , light, , , , , , , , , no strings, , , , , , , , , scary, , , , , , , , , quick,
+
+ 
+
+men, girl, no voices, man singing, not English, female singing, talking, women, male vocals, no singer, lol, clapping, different, voices, old, slow, guitar, solo, man, voice, guitars, not opera, not classical, not rock, no beat, happy, sad, strange, no drums, woman singing, noise, female singer, no flute, low, female vocals, foreign, no voice, loud, silence, English, no singing, singing, vocals, no vocals, weird, male voice, female vocal, upbeat, quiet, , woman, male vocal, no vocal, , soft
