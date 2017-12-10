8ba38ce1273ac690d2a4b8ddabcb4f4bda5348b2
Andrew Mangeni <alm2255@columbia.edu>
2017-12-10 16:44:43 +0000

    transforming song - broken

---
+++ transform_song.py
@@ -0,0 +1,40 @@
+from pydub import AudioSegment
+from generate import main
+import time
+
+second_unit = 1000.0
+output_song_path = 'transform/generated.wav'
+seed_song_path = 'transform/seed.wav'
+new_song_path = 'transform/in_progress.wav'
+input_song_path = 'transform/input.wav'
+# 10 seconds for wavenet = 160000
+
+def transform_song(input_song):
+	output_song = None
+	song = AudioSegment.from_file(input_song, format="wav")
+	leng = len(song)
+	last_mark = 12.5*second_unit#0
+	for i in range(1):#while last_mark < leng - 500:
+		next_mark = min(leng, last_mark + 500)
+		print(next_mark)
+		print(last_mark)
+		song[last_mark:next_mark].export(seed_song_path, format="wav")
+		
+		main(seed_song_path, new_song_path, 8000)
+		new_clip = AudioSegment.from_file(new_song_path, format="wav")
+		if output_song == None:
+			output_song = new_clip
+		else:
+			output_song = output_song + new_clip
+		last_mark = next_mark
+		print('yay!')
+		time.sleep(3)
+		
+		
+	
+	output_song = output_song.fade_out()
+	output_song.export(output_song_path, format="wav")
+	print('DONE')
+		
+		
+transform_song(input_song_path)
